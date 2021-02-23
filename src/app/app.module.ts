0024cf1269d74a02a3d48b7b12c9e05da534d36f
vladislava_zh <vladislava_zh@malam.com>
2021-02-23 17:20:27 +0200

    weather

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,59 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
+import {CurrentWeatherComponent} from '../components/current-weather/current-weather.component';
+import {HomePageComponent} from '../components/home-page/home-page.component';
+import {WeatherService} from "../services/weather.service";
+import {MatListModule} from "@angular/material/list";
+import {MatDividerModule} from "@angular/material/divider";
+import {HttpClientModule} from "@angular/common/http";
+import {MatCardModule} from "@angular/material/card";
+import {MatTabsModule} from "@angular/material/tabs";
+import {MatInputModule} from "@angular/material/input";
+import {FormsModule} from "@angular/forms";
+import {locationReducer} from "../share/location-reducer";
+import {StoreModule} from "@ngrx/store";
+import {MatToolbarModule} from "@angular/material/toolbar";
+import {MatButtonModule} from "@angular/material/button";
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
+import {RouterModule, Routes} from "@angular/router";
+import {TopBarComponent} from "../components/top-bar/top-bar.component";
+
+const routes: Routes = [
+    {path: 'current-weather', component: CurrentWeatherComponent},
+    {path: 'home-page', component: HomePageComponent},
+];
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        HomePageComponent,
+        TopBarComponent,
+        CurrentWeatherComponent,
+    ],
+    imports: [
+        BrowserModule,
+        // AppRoutingModule,
+        BrowserAnimationsModule,
+        MatButtonModule,
+        MatToolbarModule,
+        StoreModule.forRoot({
+            loc: locationReducer
+        }),
+        RouterModule.forRoot(routes),
+        FormsModule,
+        MatInputModule,
+        MatTabsModule,
+        MatCardModule,
+        HttpClientModule,
+        MatDividerModule,
+        MatListModule
+    ],
+    providers: [
+        WeatherService
+    ],
+    bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
-  production: true
+  production: true,
+  apiKey: '0d7303c17ee3d3482cd82a2ad273a90d',
+  apiUrl: 'http://api.openweathermap.org/data/2.5'
 };
